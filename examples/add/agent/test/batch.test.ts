e3fd61d5c16d9311a0d981df7c7f6e4019fb01bf
DFST <mk@dfst.io>
2025-09-07 20:07:12 +0300

    reserved proofs

--- examples/add/agent/test/batch.test.ts
+++ examples/add/agent/test/batch.test.ts
@@ -10,7 +10,7 @@ describe("Batch", async () => {
 
     while (true) {
       batchIteration++;
-      const maxDelay = Math.floor(Math.random() * 25 * 1000); //20 - 0.1 TPS
+      const maxDelay = Math.floor(Math.random() * 30 * 1000); //20 - 0.1 TPS
 
       // Calculate TPS
       const elapsedSeconds = (Date.now() - testStartTime) / 1000;
