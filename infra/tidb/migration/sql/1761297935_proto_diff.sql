61bb5c226aaf86f53cb4020ef10cc57aa540238a
DFST <mk@dfst.io>
2025-10-24 22:31:23 +0300

    devnet redeployment

--- infra/tidb/migration/sql/1761297935_proto_diff.sql
+++
@@ -1,4 +0,0 @@
--- dry run --
-ALTER TABLE `proof_event` ADD COLUMN `settlement_proof_chain` varchar(255) NULL AFTER `settlement_proof`;
-ALTER TABLE `proof_event` ADD INDEX `idx_aii_bn_sp_spc` (`app_instance_id`, `block_number`, `settlement_proof`, `settlement_proof_chain`);
-ALTER TABLE `proof_event` DROP INDEX `idx_app_instance_id_block_number_settlement_proof`;
